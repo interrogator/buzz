134e14dd2cdb84fc588a0d465b4df4b216bd9818
Danny McDonald <mcddjx@gmail.com>
2019-06-09 19:18:17 +0200

    try benepar later

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
 from setuptools import setup
-from setuptools.command.install import install
-
-import os
-from os.path import isfile, isdir, join, dirname
-
-class CustomInstallCommand(install):
-    """
-    Customized setuptools install command, which installs
-    some NLTK data automatically
-    """
-    def run(self):
-        from setuptools.command.install import install
-        try:
-            import cython
-        except ImportError:
-            os.system('pip install cython')
-        try:
-            import numpy
-        except ImportError:
-            os.system('pip install numpy') 
-        import site
-        try:
-            reload(site)
-        except NameError:
-            pass
-        install.run(self)
 
 
 setup(name='buzz',
@@ -36,8 +10,6 @@ setup(name='buzz',
       zip_safe=False,
       packages=['buzz'],
       scripts=['buzz/parse'],
-      setup_requires=['cython', 'numpy'],
-      cmdclass={'install': CustomInstallCommand,},
       package_data={'buzz': ['*.sh',
                              'buzz/*.sh',
                              '*.p',
@@ -51,8 +23,8 @@ setup(name='buzz',
                         'bllipparser',
                         'scipy',
                         'cython',
-                        'benepar',
-                        'benepar[cpu]',
+                        # 'benepar',
+                        # 'benepar[cpu]',
                         'tensorflow==1.12.0',
                         'spacy',
                         'pandas',
