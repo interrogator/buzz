1dd898e9a6ad1cc0d9cbe052d578f310834a33b4
Danny McDonald <mcddjx@gmail.com>
2019-06-11 12:06:43 +0200

    first working depgrep and tgrep

--- buzz/search.py
+++ buzz/search.py
@@ -3,7 +3,7 @@ from nltk.tgrep import tgrep_compile
 
 from .query import depgrep_compile
 
-from .utils import _make_tree, _get_tqdm, _tqdm_update, _tqdm_close
+from .utils import _make_tree, _get_tqdm, _tqdm_update, _tqdm_close, _tree_once
 
 
 class Searcher(object):
@@ -12,174 +12,91 @@ class Searcher(object):
     """
 
     def __init__(self, corpus):
-        # really hate this code...
+        """
+        Searcher understands Corpus, File and Dataset
+        """
         from .file import File
         from .corpus import Corpus
         from .dataset import Dataset
-
         self.corpus = corpus
-        # if we can't bring corpus into memory, we will use files
-        if type(corpus) == Corpus and self.corpus.too_large_for_memory:
-            print('Warning: corpus too large for memory!')
-            self.to_search = corpus.files
+        if type(corpus) == Corpus:
+            self.to_search = self.corpus.files
             self.reference = None
-            return
-        # if corpus is unloaded or a file, load it
-        if type(corpus) in {File, Corpus}:
-            self.corpus = corpus.load()
-            self.reference = self.corpus  # .copy()
+        elif type(corpus) == File:
+            self.to_search = [self.load()]
+            self.corpus = self.load()
+            self.reference = self.corpus.refence
         # if it's results, use the reference of that
         elif type(corpus) == Dataset:
-            self.corpus = corpus._df()
-            self.reference = corpus.reference
-        # if it's just a dataframe, we can guess...
-        elif isinstance(corpus, pd.DataFrame):
-            self.reference = corpus
-        self.to_search = [self.corpus]
+            self.to_search = [self.corpus]
+            self.reference = self.corpus.reference
 
-    def tgrep_searcher(self, countmode=False, multiprocess=0):
+    def _tgrep_iteration(self, df):
         """
-        Search a DataFrame using tgrep.
+        Search a DataFrame-like object's parse column using tgrep.
         """
-        df = self.corpus
         df['_gram'] = False
-
-        if isinstance(self.query, (str, bool)):
-            self.query = tgrep_compile(self.query)
-
-        if isinstance(self.corpus, pd.DataFrame):
-            tqdm = _get_tqdm()
-
-        tree_once = self.corpus.tree_once()
+        tree_once = _tree_once(df)
         if isinstance(tree_once.values[0], str):
             tree_once = tree_once.apply(_make_tree)
 
-        ser = list()
-        six = list()
+        # results go here
+        gram_column_strings, indices_to_keep = list(), list()
 
+        # progbar when possible
         if isinstance(self.corpus, pd.DataFrame):
+            tqdm = _get_tqdm()
             running_count = 0
             t = tqdm(
                 total=len(tree_once),
                 desc='Searching trees',
-                position=multiprocess,
                 ncols=120,
                 unit='tree',
             )
 
-        # broken
-        # should do dropna or whatever on this
         for n, tree in tree_once.iteritems():
             if not tree:
                 continue
             match_count = 0
+            # a tree is a bunch of positions. we iterate over each and check for match there
             root_positions = tree.treepositions(order='leaves')
             positions = tree.treepositions()
             for position in positions:
                 if self.query(tree[position]):
                     match_count += 1
-                    size = len(tree[position].leaves())
+                    size = len(tree[position].leaves())  # how long is match for _gram
                     first = tree[position].treepositions('leaves')[0]
                     first = position + first
                     pos = root_positions.index(first)
+                    # construct how the _gram looks
                     form = ','.join([str(x) for x in range(pos + 1, pos + size + 1)])
-                    ser.append(form)
-                    six.append(n + pos)
+                    gram_column_strings.append(form)
+                    # make index of this match and remember it
+                    index_of_this_match = (n[0], n[1], pos+1)
+                    indices_to_keep.append(index_of_this_match)
+
+            # progress bar stuff for df
             if isinstance(self.corpus, pd.DataFrame):
                 running_count += match_count
                 kwa = dict(results=format(running_count, ','))
                 t.set_postfix(**kwa)
                 t.update()
 
-        if isinstance(self.corpus, pd.DataFrame):
-            t.close()
+        _tqdm_close(t)
 
-        df = df.iloc[six].copy()
-        df['_gram'] = ser
-        return df[['w', '_gram']]
-        return df
-
-    @staticmethod
-    def number_query(df, target, query):
-        op, x = getattr(query, 'pattern', query).strip().split(' ', 1)
-        op_no_negation = op.lstrip('!')
-        try:
-            x = float(x)
-        except Exception:  # todo: why?
-            pass
-        if op_no_negation in {'=', '=='}:
-            crit = df[target] == x
-        if op_no_negation == '<':
-            crit = df[target] < x
-        if op_no_negation == '<=':
-            crit = df[target] <= x
-        if op_no_negation == '>':
-            crit = df[target] > x
-        if op_no_negation == '>=':
-            crit = df[target] >= x
-        # invert if negation
-        if op.startswith('!'):
-            return ~crit
-        return crit
-
-    def query_a_piece(self, piece, usecols):
-        """
-        Return bool index for match/no match for each token in this piece
-        """
-        if not isinstance(piece, pd.DataFrame):
-            piece = piece.load()
-
-        if self.target in piece.index.names:
-            piece[self.target] = piece.index.get_level_values(self.target)
-
-        num_query = self.target in {'i', 'g', 'sent_len'}
-
-        # dependency search
-        if self.target == 'd':
-            # make multiindex and add an _n column, then remove old index
-            df = piece.drop(['_n', 'file', 's', 'i'], axis=1, errors='ignore')
-            df['_n'] = range(len(df))
-            df = df.reset_index()
-            self.query = depgrep_compile(self.query, df=df, case_sensitive=self.case_sensitive)
-            bool_ix = self.depgrep(piece)
-        # tree search
-        elif self.target == 't':
-            bool_ix = self.tgrep_searcher()
-        # numerical query
-        elif num_query:
-            bool_ix = self.number_query(piece, self.target, self.query)['w']
-        # anything else
-        else:
-            if isinstance(self.query, (list, set)):
-                values = set(self.query)
-                if not self.case_sensitive:
-                    values = {i.lower() for i in values}
-                    strings = piece[self.target].astype(str).str.lower()
-                else:
-                    strings = piece[self.target].astype(str)
-                bool_ix = strings.isin(values)
-            else:
-                kwa = dict(self.query, case=self.case_sensitive, regex=self.regex)
-                bool_ix = piece[self.target].str.contains(**kwa)
-            # invert if we want to
-            if self.inverse:
-                bool_ix = ~bool_ix
-        return piece['_n'][bool_ix]
+        df = df.loc[indices_to_keep]  #.copy()
+        df['_gram'] = gram_column_strings
+        return df['_gram']
 
     def depgrep(self, df):
         """
         Run query over dependencies
         """
-        # todo: get the full dataframe
-        raise NotImplementedError()
-        df = self.corpus
-
         # create progress bar
-        if isinstance(self.corpus, pd.DataFrame):
+        if isinstance(df, pd.DataFrame):
             tqdm = _get_tqdm()
             prog_bar_info = dict(desc='Searching loaded corpus', unit='tokens', ncols=120)
             tqdm.pandas(**prog_bar_info)
-
             matches = df.progress_apply(self.query, axis=1)
         # when corpus is not loaded, no progress bar?
         else:
@@ -190,76 +107,71 @@ class Searcher(object):
         except Exception:  # todo: why?
             pass
 
-        bools = [bool(i) for i in matches.values]
-        return bools
+        return [bool(i) for i in matches.values]
 
-    def run(
-        self,
-        target,
-        query=False,
-        multiprocess=False,
-        case_sensitive=False,
-        load=True,
-        regex=True,
-        inverse=False,
-        **kwargs
-    ):
+    def _depgrep_iteration(self, piece):
+        """
+        depgrep over one piece of data, returning the matching lines
+        """
+        # make multiindex and add an _n column, then remove old index
+        df = piece.drop(['_n', 'file', 's', 'i'], axis=1, errors='ignore')
+        df['_n'] = range(len(df))
+        df = df.reset_index()
+        # comppile the query against this dataframe
+        self.query = depgrep_compile(self.query, df=df, case_sensitive=self.case_sensitive)
+        # run the query, returning a boolean index
+        bool_ix = self.depgrep(piece)
+        # get just the lines matching the bool ix
+        return bool_ix
 
+    def run(self, target, query, case_sensitive=True):
+        """
+        Search either trees or dependencies for query
+
+        Return: Dataset of matching indices
+        """
         from .file import File
+        from .dataset import Dataset
 
-        if target.startswith('t') and inverse:
-            raise NotImplementedError('Cannot do this yet')
-
-        self.inverse = inverse
-        self.regex = regex
         self.target = target
-        self.case_sensitive = case_sensitive
         self.query = query
+        self.case_sensitive = case_sensitive
 
-        if isinstance(query, str):
-            reg = query.startswith('/') and query.endswith('/')
-            sim = query.startswith(('"', "'")) and query.endswith(('"', "'"))
-            if sim or reg:
-                self.query = self.query[1:-1]
+        # where we store our results...
+        results = list()
 
-        if regex is not True:
-            regex = True if reg else False
-
+        # unlike depgrep, tgrep queries are compiled without the file data, so can be done once
         if target == 't':
             self.query = tgrep_compile(query)
 
-        results = list()
-
-        # just load the target column coz we are bad ass
-        usecols = ['file', 's', 'i', target]
-
+        # progbar stuff
         tqdm = _get_tqdm()
         kwa = dict(
             total=len(self.to_search),
             desc='Searching corpus',
-            position=multiprocess,
             ncols=120,
             unit='document',
         )
-        simple = self.target not in {'t', 'd'}
-        t = tqdm(**kwa) if len(self.to_search) > 1 and simple else None
+        t = tqdm(**kwa) if len(self.to_search) > 1 else None
 
+        # iterate over searchable bits, doing query with progbar
         for piece in self.to_search:
             if isinstance(piece, File):
                 piece = piece.load()
-            res = self.query_a_piece(piece, usecols)
+            # do the dep or tree searches and make a reduced dataset containing just matches
+            if self.target == 'd':
+                res = piece[self._depgrep_iteration(piece)]
+            elif self.target == 't':
+                gram_ser = self._tgrep_iteration(piece)
+                res = piece.loc[gram_ser.index]
+                res['_gram'] = gram_ser
+
             if not res.empty:
                 results.append(res)
             _tqdm_update(t)
         _tqdm_close(t)
 
-        from .table import Table
-
-        if not results:
-            print('No results, sorry.')
-            results = Table()
-        else:
-            results = pd.concat(results, sort=False)
-            results = Table(results)
+        results = Dataset() if not results else Dataset(pd.concat(results, sort=False))
+        # if we already had reference corpus, it can stay...
         results.reference = self.reference
         return results
