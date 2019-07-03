4ff99c9bc8a9fbc57f4484741832df5a8f632281
Danny McDonald <mcddjx@gmail.com>
2019-07-03 15:12:30 +0200

    Bump version: 2.0.0 → 2.0.1

--- buzz/__init__.py
+++ buzz/__init__.py
@@ -11,4 +11,4 @@ warnings.filterwarnings("ignore", message="Attribibute 'is_copy")
 warnings.filterwarnings("ignore")
 
 
-__version__ = "__version__ = '2.0.0'"
+__version__ = "__version__ = '__version__ = "2.0.1"'"
