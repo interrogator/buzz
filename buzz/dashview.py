bb0a97f6d709d2e86ba5f6adffbd59afb11eb25e
Danny McDonald <mcddjx@gmail.com>
2019-08-04 23:51:00 +0200

    unused imports

--- buzz/dashview.py
+++ buzz/dashview.py
@@ -2,8 +2,6 @@ from multiprocessing import Process
 
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 
 import dash
