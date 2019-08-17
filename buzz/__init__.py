84db080e5d241d0d5cf9c3bb3fe81448605eccf2
Danny McDonald <mcddjx@gmail.com>
2019-08-17 16:46:33 +0200

    Bump version: 2.0.5 → 3.0.0

--- buzz/__init__.py
+++ buzz/__init__.py
@@ -18,4 +18,4 @@ warnings.filterwarnings("ignore", message="Attribibute 'is_copy")
 warnings.filterwarnings("ignore")
 
 
-__version__ = "2.0.5"
+__version__ = "3.0.0"
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="buzz",
-    version="2.0.5",  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+    version="3.0.0",  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
     description="Sophisticated corpus linguistics",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
